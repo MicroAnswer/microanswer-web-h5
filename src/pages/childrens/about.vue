1787b2b0960ee8dfe9ea43e7abd83371ccc52851
Microanswer <microanswer@outlook.com>
2019-09-15 23:26:26 +0800

    优化框架。关于界面加入。

--- src/pages/childrens/about.vue
+++ src/pages/childrens/about.vue
@@ -1,9 +1,65 @@
 <template>
-    <div>关于</div>
-</template>
+    <div style="padding: 8px">
 
-<script>
-    export default {
-        name: "about"
+        <!-- 顶部说明 -->
+        <md-card>
+            <md-card-header>
+                <div class="md-title">Microanswer的个人网站</div>
+                <div class="md-subhead">移动web版</div>
+            </md-card-header>
+
+            <md-card-content>
+                <p>本站内容均为博主原创，若发现存在恶意抄袭情况，请邮箱博主，博主将第一时间删除并致歉。本站提供了博客、专题、产品版块，记录了博主的涉猎范围以及作品展示。</p>
+                <p class="md-caption"><span class="md-title">博客</span>：新知识博文、疑难解决方案博文通常将出现在此版块下，每一个博客，除非未特别指定，均默认允许所有已登录用户进行评论，允许所有浏览用户点赞。博主希望每一位浏览过博客的人都留下一个赞，每次赞的冷却 时间为20小时，必须等待20小时后才能再一次进行赞。比起赞，博主更喜欢评论，因为这样可以为站点带来用户量。</p>
+                <p class="md-caption"><span class="md-title">专题</span>：有一套完整体系、提供全面的知识或技能介绍的版块，每一个专题都有其介绍的完整功能，它的具体内容本质上也是博文，不过这些博文不会展现在博文列表里，而是只能在专题里寻找到。专题支持点赞评论。</p>
+                <p class="md-caption"><span class="md-title">产品</span>：博主个人产出的作品代表，包含但不限于公示的所有内容。</p>
+                <p class="md-caption"><span class="md-title">留言</span>：本站不提供留言功能，但你仍然可以发表你的意见，博主建议你可以到下方[更新历史]版块里，发表对本站的观点和评论。</p>
+                <p>博主能力有限，若发现站内出现或存在BUG，请邮箱至博主，邮箱地址见本页末尾处。本站欢迎友链加入，你可以直接邮箱博主，注明你的网址和网站名称，博主很快便会处理。</p>
+            </md-card-content>
+        </md-card>
+
+        <!-- 更新记录 -->
+        <md-card style="margin-top: 8px">
+            <md-card-header>
+                <div class="md-title">更新记录</div>
+            </md-card-header>
+
+            <md-card-content>
+                <md-button class="md-raised md-dense" href="">点击查看更新记录</md-button>
+            </md-card-content>
+        </md-card>
+
+        <!-- 友情链接 -->
+        <md-card style="margin-top: 8px">
+            <md-card-header>
+                <div class="md-title">友情链接</div>
+            </md-card-header>
+
+            <md-card-content>
+                <a href=";">少司命</a>、
+                <a href=";">程序猿童先森</a>
+            </md-card-content>
+        </md-card>
+
+        <!-- 邮箱 -->
+        <md-card style="margin-top: 8px">
+            <md-card-header>
+                <div class="md-title">博主邮箱</div>
+            </md-card-header>
+
+            <md-card-content>
+                <a href="mailto:374288225@qq.com">374288225@qq.com</a>、
+                <a href="mailto:microanswer@outlook.com">microanswer@outlook.com</a>
+            </md-card-content>
+        </md-card>
+        <p>
+            <a href="http://beian.miit.gov.cn/" target="_blank">蜀ICP备17035828号</a>
+            · Page width <a href="https://vuematerial.io" target="_blank">vue-material</a> by Microanswer。
+        </p>
+    </div>
+</template>
+<style scoped>
+    p{
+        margin-top: 8px;
     }
-</script>
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
+    <div class="m-w-container">
 
         <!-- 顶部ToolBar -->
-        <md-toolbar class="md-primary">
+        <md-toolbar class="md-primary m-w-toolbar">
             <div class="md-toolbar-row">
                 <md-button class="md-icon-button" @click="showNavigation=true">
                     <md-icon>menu</md-icon>
@@ -15,45 +15,46 @@
         <router-view/>
 
         <!-- 侧边菜单 -->
-        <md-drawer :md-active.sync="showNavigation" md-swipeable>
+        <md-drawer :md-active.sync="showNavigation" md-swipeable md-fixed>
 
 
-            <md-content class="md-primary">
+            <md-content class="md-primary" style="padding-top: 30px;padding-bottom: 20px;">
 
-                <md-avatar class="md-large">
-                    <md-icon>home</md-icon>
+                <md-avatar class="md-large" style="background-color: wheat;margin-left: 8px">
+                    <md-icon>person</md-icon>
                 </md-avatar>
 
-                <router-link style="color: wheat" to="./login">登录</router-link>
-                /
-                <router-link style="color: wheat" to="./reg">注册</router-link>
+                <md-content style="display: inline-block;vertical-align: middle;background-color: transparent">
+                <md-button class="md-raised md-accent" to="./login">登录</md-button>
+                <md-button class="md-raised" to="./reg">注册</md-button>
+                </md-content>
 
             </md-content>
 
 
             <md-list>
-                <md-list-item to="./">
+                <md-list-item :class="$route.path === '/' && 'router-link-active'" @click="onDrawerListItemClick('./')">
                     <md-icon>home</md-icon>
                     <span class="md-list-item-text">首页</span>
                 </md-list-item>
 
-                <md-list-item to="./albums">
-                    <md-icon>send</md-icon>
-                    <span class="md-list-item-text">专题</span>
-                </md-list-item>
-
-                <md-list-item to="./blogs">
-                    <md-icon>delete</md-icon>
+                <md-list-item :class="$route.path === '/blogs' && 'router-link-active'" @click="onDrawerListItemClick('./blogs')">
+                    <md-icon>description</md-icon>
                     <span class="md-list-item-text">博客</span>
                 </md-list-item>
 
-                <md-list-item to="./apps">
-                    <md-icon>error</md-icon>
+                <md-list-item :class="$route.path === '/albums' && 'router-link-active'" @click="onDrawerListItemClick('./albums')">
+                    <md-icon>toys</md-icon>
+                    <span class="md-list-item-text">专题</span>
+                </md-list-item>
+
+                <md-list-item :class="$route.path === '/apps' && 'router-link-active'" @click="onDrawerListItemClick('./apps')">
+                    <md-icon>extension</md-icon>
                     <span class="md-list-item-text">产品</span>
                 </md-list-item>
 
-                <md-list-item to="./about">
-                    <md-icon>error</md-icon>
+                <md-list-item :class="$route.path === '/about' && 'router-link-active'" @click="onDrawerListItemClick('./about')">
+                    <md-icon>info</md-icon>
                     <span class="md-list-item-text">关于</span>
                 </md-list-item>
             </md-list>
@@ -66,6 +67,14 @@
 
     export default {
         name: "index",
+        methods: {
+            onDrawerListItemClick (to) {
+                if (to.replace('.', '') !== this.$route.path) {
+                    this.$router.push(to);
+                }
+                this.showNavigation = false;
+            }
+        },
         data() {
             return {
                 showNavigation: false
@@ -73,3 +82,17 @@
         }
     }
 </script>
+<style scoped>
+    .m-w-container {padding-top: 64px;}
+    @media (max-width: 960px) {.m-w-container {padding-top: 48px;}}
+    @media (max-width: 600px) {.m-w-container {padding-top: 56px;}}
+    .m-w-toolbar {
+        position: fixed;
+        top: 0;
+        right: 0;
+        left: 0;
+        z-index: 3;
+        margin: auto;
+        max-width: 100%;
+    }
+</style>
